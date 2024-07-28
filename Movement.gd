b8665a79fe7c4c53668f38ade8c08f7ad354bc99
rashley <verdicaysen@gmail.com>
2024-07-28 14:43:59 -0500

    Updated to work the way we wanted.

--- Movement.gd
+++ Movement.gd
@@ -1,14 +1,16 @@
 extends RigidBody2D
 
+var flySpeed = 50
 
+func _ready():
+	print(flySpeed)
 #This is for movement.
-func _Movement(delta):
-	var flySpeed = 500
+func _physics_process(delta):
 	if Input.is_action_pressed("up"):
-		apply_force(Vector2(0,-25))
+		apply_force(Vector2(0, -flySpeed))
 	if Input.is_action_pressed("down"):
-		apply_force(Vector2(0,25))
+		apply_force(Vector2(0, flySpeed))
 	if Input.is_action_pressed("left"):
-		apply_force(Vector2(-25,0))
+		apply_force(Vector2(-flySpeed, 0))
 	if Input.is_action_pressed("right"):
-		apply_force(Vector2(25,0))
+		apply_force(Vector2(flySpeed, 0))
